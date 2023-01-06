90de3c7c8e0e672019b61d21f6699bc330f9707c
Rafael <rafaeloliveiracds@gmail.com>
2023-01-06 04:37:30 -0300

    [ feat backend ] decryptPassword

--- backend/src/utils/crypt.ts
+++ backend/src/utils/crypt.ts
@@ -1,8 +1,13 @@
-import { genSaltSync, hashSync } from 'bcrypt';
+import { compareSync, genSaltSync, hashSync } from 'bcrypt';
 
 export const encryptPassword = (password: string) => {
 	const saltRounds = 10;
 	const salt = genSaltSync(saltRounds);
 	const hash = hashSync(password, salt);
 	return hash;
+};
+
+export const decryptPassword = (inputPassword: string, userPassword: string) => {
+	const isSamePassword = compareSync(inputPassword, userPassword);
+	return !isSamePassword;
 };
