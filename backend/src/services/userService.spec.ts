9b66406b4a1ef52de5a26b21d19d24d1df570707
Rafael <rafaeloliveiracds@gmail.com>
2023-01-06 08:07:06 -0300

    [ test backend ] user service Login

--- backend/src/services/userService.spec.ts
+++ backend/src/services/userService.spec.ts
@@ -1,12 +1,16 @@
 import { describe, expect, it } from 'vitest';
 import { UserRepository } from '../repositories/userRepository';
 import { UserType } from '../schemas/types/userType';
+import { encryptPassword } from '../utils/crypt';
 import { UserService } from './userService';
 
 class UserRepositoryMock implements UserRepository {
 	private users = [{ email: 'teste@teste.com', name: 'teste', password: '123' }];
 
 	async createUser(user: UserType) {
+		const hash = encryptPassword(user.password);
+		user.password = hash;
+		this.users.push(user);
 		return;
 	}
 
@@ -18,30 +22,65 @@ class UserRepositoryMock implements UserRepository {
 describe('User Service', () => {
 	const userService = new UserService(new UserRepositoryMock());
 
-	it('creates a new User', async () => {
-		const user = { email: 'teste@teste.com', name: 'teste', password: '123' };
-		const response = await userService.createUser({ user });
+	describe('Create', () => {
+		it('creates a new User', async () => {
+			const user = { email: 'teste@teste.com', name: 'teste', password: '123' };
+			const response = await userService.createUser({ user });
 
-		expect(response).toHaveProperty('message');
-		expect(response.message).toBe('New user created');
+			expect(response).toHaveProperty('message');
+			expect(response.message).toBe('New user created');
+		});
 	});
 
-	it('returns a user', async () => {
-		const email = 'teste@teste.com';
-		const response = await userService.readUser({ email });
+	describe('Read', () => {
+		it('returns a user', async () => {
+			const email = 'teste@teste.com';
+			const response = await userService.readUser({ email });
 
-		expect(response.user.email).toBe(email);
-		expect(response.user.password).toBe('');
-		expect(response.message).toBe('User found');
+			expect(response.user.email).toBe(email);
+			expect(response.user.password).toBe('');
+			expect(response.message).toBe('User found');
+		});
+
+		it('Returns a empty user', async () => {
+			const email = 'dummy@teste.com';
+			const response = await userService.readUser({ email });
+
+			expect(response.user).toBeUndefined();
+			expect(response.message).toBe('User not found');
+		});
 	});
 
-	it('Returns a empty user', async () => {
-		const email = 'dummy@teste.com';
-		const response = await userService.readUser({ email });
+	describe('Login', () => {
+		it('login a user', async () => {
+			const user = { email: 'teste2@teste.com', password: '123' };
+			await userService.createUser({ user: { ...user, name: 'teste' } });
+			const response = await userService.login({ user });
 
-		expect(response.user).toBeUndefined();
-		expect(response.message).toBe('User not found');
+			expect(response.message).toBe('Login');
+			expect(response.token).toBeTruthy();
+		});
+
+		it('Email is wrong, login should fail', async () => {
+			const user = { email: 'teste3@teste.com', password: '123' };
+			await userService.createUser({ user: { ...user, name: 'teste' } });
+
+			try {
+				await userService.login({ user: { email: 'wrong@test.com', password: '123' } });
+			} catch (error: any) {
+				expect(error.message).toBe('Email/password is wrong');
+			}
+		});
+
+		it('Password is wrong, login should fail', async () => {
+			const user = { email: 'teste@teste.com', password: '123' };
+			await userService.createUser({ user: { ...user, name: 'teste' } });
+
+			try {
+				await userService.login({ user: { email: 'teste@teste.com', password: 'wrong' } });
+			} catch (error: any) {
+				expect(error.message).toBe('Email/password is wrong');
+			}
+		});
 	});
-
-	it.todo('login a user', () => {});
 });
