14dee7ef8faa281b822fe75e805f82db4def1407
Rafael <rafaeloliveiracds@gmail.com>
2023-01-06 04:53:18 -0300

    [ feat backend ] apollo server formatted error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,11 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { schema } from './schemas/schema';
 
-const app = new ApolloServer({ schema });
+const app = new ApolloServer({
+	schema,
+
+	formatError: error => ({ message: error.message }),
+});
 
 export const startServer = async () => {
 	const { url } = await startStandaloneServer(app, {
