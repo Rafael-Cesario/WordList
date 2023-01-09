b3d69a09b2e88e9957aae5538165286e4aaac86c
Rafael <rafaeloliveiracds@gmail.com>
2023-01-09 19:29:06 -0300

    [ feat frontend ] query types

---
+++ frontend/services/queriesTypes.ts
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client';
+
+export const READ_USER = gql`
+	query ReadUser($email: String!) {
+		readUser(email: $email) {
+			message
+			user {
+				email
+				name
+				password
+			}
+		}
+	}
+`;
+
+export const LOGIN = gql`
+	mutation Login($user: LoginInput!) {
+		login(user: $user) {
+			message
+			token
+		}
+	}
+`;
+
+export const CREATE_USER = gql`
+	mutation CreateUser($user: UserInput!) {
+		createUser(user: $user) {
+			message
+			user {
+				email
+				name
+				password
+			}
+		}
+	}
+`;
