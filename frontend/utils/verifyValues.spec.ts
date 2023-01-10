5a23ee300b69797c84bd23f703d06b55c496c7cd
Rafael <rafaeloliveiracds@gmail.com>
2023-01-10 10:46:03 -0300

    [ tests frontend ] verify values

--- frontend/utils/verifyValues.spec.ts
+++ frontend/utils/verifyValues.spec.ts
@@ -1,7 +1,22 @@
 import { describe, test, expect } from 'vitest';
+import { verifyValues } from './verifyValues';
+
+const sendErrorMock = (error: string) => {
+	return error;
+};
+
+const removeErrorMock = (error: string) => {
+	return error;
+};
 
 describe('Utils, verify Values', () => {
-	test('blabla bla', () => {
-		expect(2 + 2).toBe(4);
+	test('Return false if there is no errors', () => {
+		const errors = verifyValues({ email: undefined, password: undefined }, sendErrorMock, removeErrorMock);
+		expect(errors).toBe(false);
+	});
+
+	test('Return true if there is a error', () => {
+		const errors = verifyValues({ email: 'error here ' }, sendErrorMock, removeErrorMock);
+		expect(errors).toBe(true);
 	});
 });
