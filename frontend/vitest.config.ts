c690198871bb63b9c2ad584e83dd25c226fc3d64
Rafael <rafaeloliveiracds@gmail.com>
2023-01-10 10:40:37 -0300

    [ config frontend ] vitest config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,8 +1,11 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+	plugins: [react()],
 	test: {
 		watch: false,
 		reporters: 'verbose',
+		environment: 'jsdom',
 	},
 });
