01d41acaee0b04fd817d68ec8a00aaecd0ad3bc8
Rafael <rafaeloliveiracds@gmail.com>
2023-01-10 13:43:06 -0300

    [ tests frontend ] utils error, remove error

--- frontend/utils/error.spec.tsx
+++ frontend/utils/error.spec.tsx
@@ -1,12 +1,14 @@
 import '@testing-library/jest-dom';
 import { describe, test, expect } from 'vitest';
-import { sendError } from './error';
+import { removeError, sendError } from './error';
 import { render, screen } from '@testing-library/react';
 
 const DummyRender = () => {
 	return (
 		<div>
-			<label htmlFor='email'>Email</label>
+			<label htmlFor='email' data-name='Email'>
+				Email
+			</label>
 			<input type='text' id='email' placeholder='Email' />
 		</div>
 	);
@@ -23,5 +25,18 @@ describe('Utils, error', () => {
 
 		expect(label).toHaveTextContent('Error here');
 		expect(divClass).toBe('error');
+	});
+
+	test('Remove the style and change back the label text', () => {
+		render(<DummyRender />);
+		sendError('email: Error here');
+		removeError('email');
+
+		const input = screen.getByPlaceholderText('Email');
+		const label = screen.getByText('Email');
+		const divClass = input.parentElement?.className;
+
+		expect(label).toHaveTextContent('Email');
+		expect(divClass).toBe('');
 	});
 });
