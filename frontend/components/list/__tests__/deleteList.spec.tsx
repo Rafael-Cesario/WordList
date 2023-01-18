45642bfd719dcf8c2b1da7e9734dfcc3484f9af3
Rafael <rafaeloliveiracds@gmail.com>
2023-01-17 21:22:15 -0300

    [ refactor tests ] delete list test

--- frontend/components/list/__tests__/deleteList.spec.tsx
+++ frontend/components/list/__tests__/deleteList.spec.tsx
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom';
-import { server } from '../../../services/mocks/server';
 import { describe, test, expect, vi } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { DeleteList } from '../deleteList';
@@ -8,10 +7,6 @@ vi.mock('next/router', () => require('next-router-mock'));
 vi.mock('next/dist/client/router', () => require('next-router-mock'));
 
 describe('Delete List Component', () => {
-	beforeAll(() => server.listen());
-	afterEach(() => server.resetHandlers());
-	afterAll(() => server.close());
-
 	const listName = 'Test';
 
 	test('Show confirm button', () => {
@@ -21,14 +16,5 @@ describe('Delete List Component', () => {
 		fireEvent.click(button);
 
 		expect(screen.getByText('Deletar lista Test ?')).toBeInTheDocument();
-	});
-
-	test('Delete list', () => {
-		render(<DeleteList props={{ listName }} />);
-
-		fireEvent.click(screen.getByRole('button', { name: 'Deletar lista' }));
-
-		const deleteBTN = screen.getByRole('button', { name: 'Sim' });
-		fireEvent.click(deleteBTN);
 	});
 });
