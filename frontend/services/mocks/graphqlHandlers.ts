007a7548726aa9da8877d11c388a45e9cacaa73a
Rafael <rafaeloliveiracds@gmail.com>
2023-01-18 05:40:10 -0300

    [ tests ] mock graphql handlers

--- frontend/services/mocks/graphqlHandlers.ts
+++ frontend/services/mocks/graphqlHandlers.ts
@@ -1,13 +1,23 @@
 import { graphql } from 'msw';
 
 export const handlers = [
+	graphql.mutation('CreateList', (req, res, ctx) => {
+		return res(ctx.data({ createList: { message: 'List created' } }));
+	}),
+
+	graphql.mutation('GetLists', (req, res, ctx) => {
+		return res(ctx.data({ getLists: { lists: ['list01', 'list02'] } }));
+	}),
+
+	graphql.mutation('ChangeListName', (req, res, ctx) => {
+		return res(ctx.data({ changeListName: { message: 'list name changed' } }));
+	}),
+
 	graphql.mutation('DeleteList', (req, res, ctx) => {
-		return res(
-			ctx.data({
-				deleteList: {
-					message: 'list deleted',
-				},
-			})
-		);
+		return res(ctx.data({ deleteList: { message: 'list deleted' } }));
+	}),
+
+	graphql.mutation('CreateWordList', (req, res, ctx) => {
+		return res(ctx.data({ createWordList: { message: 'Word list created' } }));
 	}),
 ];
