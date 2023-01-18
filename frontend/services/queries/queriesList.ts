d5efea4316026e23afcf7a4b958fad09739acc93
Rafael <rafaeloliveiracds@gmail.com>
2023-01-18 06:44:45 -0300

    [ refactor ] return error as obj
    refactor line break

--- frontend/services/queries/queriesList.ts
+++ frontend/services/queries/queriesList.ts
@@ -6,10 +6,7 @@ import { CHANGE_LIST_NAME, CREATE_LIST, CREATE_WORD_LIST, DELETE_LIST, GET_LISTS
 class QueriesList {
 	async createList(newList: ListType) {
 		try {
-			const response = await client.mutate({
-				mutation: CREATE_LIST,
-				variables: { newList },
-			});
+			const response = await client.mutate({ mutation: CREATE_LIST, variables: { newList } });
 			return response.data.createList;
 		} catch (error: any) {
 			return { error: error.message };
@@ -18,52 +15,37 @@ class QueriesList {
 
 	async getLists(owner: string) {
 		try {
-			const response = await client.query({
-				query: GET_LISTS,
-				variables: { owner },
-			});
-			return response.data.getLists.lists;
+			const response = await client.query({ query: GET_LISTS, variables: { owner } });
+			return response.data.getLists;
 		} catch (error: any) {
-			return error.message;
+			return { error: error.message };
 		}
 	}
 
 	async changeListName(changes: ChangesInput) {
 		try {
-			const response = await client.mutate({
-				mutation: CHANGE_LIST_NAME,
-				variables: { changes },
-			});
-
+			const response = await client.mutate({ mutation: CHANGE_LIST_NAME, variables: { changes } });
 			return response.data.changeListName;
 		} catch (error: any) {
-			return error.message;
+			return { error: error.message };
 		}
 	}
 
 	async deleteList(deleteListArgs: DeleteListInput) {
 		try {
-			const response = await client.mutate({
-				mutation: DELETE_LIST,
-				variables: { ...deleteListArgs },
-			});
-
+			const response = await client.mutate({ mutation: DELETE_LIST, variables: { ...deleteListArgs } });
 			return response.data.deleteList;
 		} catch (error: any) {
-			return error.message;
+			return { error: error.message };
 		}
 	}
 
 	async createWordList(wordList: WordListInput) {
 		try {
-			const response = await client.mutate({
-				mutation: CREATE_WORD_LIST,
-				variables: { wordList },
-			});
-
+			const response = await client.mutate({ mutation: CREATE_WORD_LIST, variables: { wordList } });
 			return response;
 		} catch (error: any) {
-			return error.message;
+			return { error: error.message };
 		}
 	}
 }
