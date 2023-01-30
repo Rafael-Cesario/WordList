af475997d37f51d8c0d84bd8815517e84bc2f251
Rafael <rafaeloliveiracds@gmail.com>
2023-01-30 18:02:11 -0300

    [ tests ] get lists

--- backend/src/__tests__/getLists.spec.ts
+++ backend/src/__tests__/getLists.spec.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { describe, beforeAll, afterAll, afterEach } from 'vitest';
+import { expect, test, describe, beforeAll, afterAll, afterEach } from 'vitest';
 import { startServer } from '../app';
 import { startDatabase } from '../database';
 import { ListModel } from '../models/listModel';
@@ -20,5 +20,24 @@ describe('Get lists query', () => {
 	afterAll(async () => {
 		await mongoose.connections[0].dropDatabase();
 		await mongoose.connections[0].close();
+	});
+
+	test('Return all the lists', async () => {
+		await queriesList.createList({ listName: 'list01', owner: 'UserEmail' });
+		await queriesList.createList({ listName: 'list02', owner: 'UserEmail' });
+
+		const response = await queriesList.getLists({ owner: 'UserEmail' });
+		const lists = response.data.getLists.lists;
+
+		expect(lists.length).toBe(2);
+		expect(lists.includes('list01')).toBe(true);
+		expect(lists.includes('list02')).toBe(true);
+	});
+
+	test('Owner has no lists', async () => {
+		const response = await queriesList.getLists({ owner: 'UserWithoutlists' });
+		const lists = response.data.getLists.lists;
+		expect(lists.length).toBe(0);
+		expect(lists).toEqual([]);
 	});
 });
