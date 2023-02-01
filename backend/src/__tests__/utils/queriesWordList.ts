32e4a533d661581e16c651986fbcba5e12241724
Rafael <rafaeloliveiracds@gmail.com>
2023-02-01 14:28:54 -0300

    [ tests ] queries wordList

--- backend/src/__tests__/utils/queriesWordList.ts
+++ backend/src/__tests__/utils/queriesWordList.ts
@@ -1,7 +1,26 @@
+import { server } from '../../app';
+import { ICreateWordList, IGetWordLists } from './interfaces/interfacesWordList';
+import { ResponseType } from './interfaces/queriesInterface';
 import { QueriesTypeWordList } from './Types/queriesTypeWordList';
 
 const queriesTypeWordList = new QueriesTypeWordList();
 
 export class QueriesWordList {
-	async getWordList(){}
+	async getWordList(getWordLists: IGetWordLists) {
+		const response = (await server.executeOperation({
+			query: queriesTypeWordList.GET_WORD_LISTS,
+			variables: { getWordLists },
+		})) as ResponseType;
+
+		return response.body.singleResult;
+	}
+
+	async createWordList(wordList: ICreateWordList) {
+		const response = (await server.executeOperation({
+			query: queriesTypeWordList.CREATE_WORD_LIST,
+			variables: { wordList },
+		})) as ResponseType;
+
+		return response.body.singleResult;
+	}
 }
