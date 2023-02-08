85cda5a5e121c83717dc761c48ffdce43f766f15
Rafael <rafaeloliveiracds@gmail.com>
2023-02-08 19:20:20 -0300

    [ tests ] find word

---
+++ frontend/components/pages/wordList/__tests__/findWord.spec.ts
@@ -0,0 +1,30 @@
+import { describe } from 'vitest';
+import { findWord } from '../utils/findWord';
+
+describe('Find word', () => {
+	test('Return true if word is present', () => {
+		const words = [
+			['word', 'word'],
+			['word', 'word'],
+			['target', 'word'],
+			['word', 'word'],
+			['word', 'word'],
+		];
+
+		const hasWord = findWord(words, 'target');
+		expect(hasWord).toBe(true);
+	});
+
+	test('Returns false, word is not present', () => {
+		const words = [
+			['word', 'word'],
+			['word', 'word'],
+			['target', 'word'],
+			['word', 'word'],
+			['word', 'word'],
+		];
+
+		const hasWord = findWord(words, 'non-existed word');
+		expect(hasWord).toBe(false);
+	});
+});
