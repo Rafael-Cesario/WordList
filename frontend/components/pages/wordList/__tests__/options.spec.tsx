5db1abf5c7fcf882fad611200ebb287fbcca7bff
Rafael <rafaeloliveiracds@gmail.com>
2023-02-09 21:44:23 -0300

    [ tests ] Save words

--- frontend/components/pages/wordList/__tests__/options.spec.tsx
+++ frontend/components/pages/wordList/__tests__/options.spec.tsx
@@ -1,4 +1,5 @@
-import { describe, test, expect, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+import { describe, test, expect, vi, beforeEach } from "vitest";
 import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { ContextWords } from "../context/contextWords";
 import { WordsContainer } from "../wordsContainer";
@@ -20,7 +21,11 @@ const renderWordsContainer = (words: string[][]) => {
 };
 
 describe("Options", () => {
-	test("Rename words args", async () => {
+	beforeEach(() => {
+		vi.resetAllMocks();
+	});
+
+	test("Try to save without changes", async () => {
 		renderWordsContainer([["word01", "word0101"]]);
 		const inputWord = screen.getByPlaceholderText("word01");
 
@@ -31,6 +36,22 @@ describe("Options", () => {
 			fireEvent.click(screen.getByText("Salvar"));
 		});
 
-		console.log(renameWords);
+		expect(renameWords).toHaveBeenCalledWith("0", { term: "", definition: "" });
+	});
+
+	test("Save Args", async () => {
+		renderWordsContainer([["word01", "word0101"]]);
+		const inputWord = screen.getByPlaceholderText("word01");
+
+		inputWord.focus();
+		await waitFor(async () => await screen.findByText("Salvar"));
+
+		await act(async () => {
+			await userEvent.clear(inputWord);
+			await userEvent.type(inputWord, "New word");
+			fireEvent.click(screen.getByText("Salvar"));
+		});
+
+		expect(renameWords).toHaveBeenCalledWith("0", { term: "New word", definition: "" });
 	});
 });
