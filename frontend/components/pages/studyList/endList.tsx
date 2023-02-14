9648290e97560924285e9ff95216cca1d4bb0a00
Rafael <rafaeloliveiracds@gmail.com>
2023-02-14 09:51:31 -0300

    [ feat ] End list component

--- frontend/components/pages/studyList/endList.tsx
+++ frontend/components/pages/studyList/endList.tsx
@@ -1,3 +1,5 @@
+import { StyledEndList } from "./styles/styledEndList";
+
 interface PropsEndList {
 	props: {
 		setHaveListEnd: (state: boolean) => void;
@@ -8,11 +10,20 @@ interface PropsEndList {
 
 export const EndList = ({ props: { setHaveListEnd, setWords, words } }: PropsEndList) => {
 	return (
-		<>
+		<StyledEndList>
 			<h1>Fim</h1>
 			<button>Estudar lista novamente</button>
 
-			<div className='words'></div>
-		</>
+			<div className='words'>
+				{words.map(([term, definition], index) => {
+					return (
+						<div key={term + index} className='word'>
+							<p>{term}</p>
+							<p>{definition}</p>
+						</div>
+					);
+				})}
+			</div>
+		</StyledEndList>
 	);
 };
---
+++ frontend/components/pages/studyList/styles/styledEndList.tsx
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const StyledEndList = styled.div``;
