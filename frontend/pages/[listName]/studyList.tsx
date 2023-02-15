b34e4651af7ca48c84745fe6834e7724631e1f33
Rafael <rafaeloliveiracds@gmail.com>
2023-02-15 11:46:12 -0300

    [ refactor ] study list, SWR words

--- frontend/pages/[listName]/studyList.tsx
+++ frontend/pages/[listName]/studyList.tsx
@@ -1,39 +1,20 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { PageHead } from "../../components/head/head";
 import { EndList } from "../../components/pages/studyList/endList";
 import { StudyContainer } from "../../components/pages/studyList/studyContainer";
 import { StyledStudyList } from "../../components/pages/studyList/styles/styledStudyList";
-import { TypeListStatus } from "../../interfaces/interfaceWordList";
-import { QueriesWords } from "../../services/queries/queriesWords";
 import { useLocalData } from "../../utils/hooks/useLocalData";
+import { useQueriesWordsSWR } from "../../utils/hooks/useQueriesWords";
 import { useRouterQuery } from "../../utils/hooks/useRouterQuery";
 
 const StudyList = () => {
-	const queriesWords = new QueriesWords();
 	const { link } = useRouterQuery("");
-	const { listIndex, listName, listStatus, owner } = useLocalData();
+	const { listIndex, listStatus } = useLocalData();
 	const [haveListEnd, setHaveListEnd] = useState(false);
-	const [words, setWords] = useState([["", ""]]);
+	const { words, isLoading } = useQueriesWordsSWR();
 
-	const getWords = async () => {
-		if (!listName) return;
-
-		const getWords = await queriesWords.getWords({
-			words: {
-				listIndex,
-				listName,
-				owner,
-				status: listStatus as TypeListStatus,
-			},
-		});
-
-		setWords(getWords.words);
-	};
-
-	useEffect(() => {
-		getWords();
-	}, [listName]);
+	if (isLoading) return <p>Carregando</p>;
 
 	return (
 		<StyledStudyList>
