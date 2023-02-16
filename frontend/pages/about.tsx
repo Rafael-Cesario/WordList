a7dd08867a8e710728a692ed7e575fa190d69db4
Rafael <rafaeloliveiracds@gmail.com>
2023-02-16 07:00:14 -0300

    [ feat ] about page

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,16 +1,36 @@
-import Head from "next/head";
+import { PageHead } from "../components/head/head";
 import Link from "next/link";
+import { StyledAbout } from "../styles/styledAbout";
 
 const Usage = () => {
 	return (
 		<div>
-			<Head>
-				<title>WordList</title>
-			</Head>
+			<PageHead title='WordList' />
 
-			<h1>Sobre</h1>
-			<p>...</p>
-			<Link href={"/main"}>Voltar</Link>
+			<StyledAbout>
+				<header>
+					<h1>WordList</h1>
+					<Link href={"/main"}>Voltar</Link>
+				</header>
+
+				<main>
+					<p>
+						WordList é um site feito para te ajudar a aprender um idioma, mais precisamente para te ajudar a memorizar algumas palavras.
+					</p>
+					<p>
+						Enquanto você estiver lendo um livro, vendo a letra de uma música ou assistindo algo, novas palavras que você ainda não
+						conhece irão aparecer, use o site para adicionar estas palavras e revisar elas mais tarde.
+					</p>
+					<p>
+						Use o site como um caderno onde você salvaria suas palavras, talvez separando elas por verbos, objetos, lugares, roupas ou
+						qualquer outra categoria que você gostaria de criar.
+					</p>
+					<span>Boas vindas ao WordList</span>
+					<Link href={"http://github.com/rafael-cesario/wordlist"} target='_blank'>
+						Visite a página no Github
+					</Link>
+				</main>
+			</StyledAbout>
 		</div>
 	);
 };
---
+++ frontend/styles/styledAbout.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const StyledAbout = styled.main`
+	margin: 2rem;
+	height: 80vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	a {
+		color: #00aaff90;
+		text-decoration: none;
+	}
+
+	header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+
+		margin: 2rem 0;
+		max-width: 500px;
+
+		p {
+			margin: 1rem 0;
+		}
+	}
+`;
