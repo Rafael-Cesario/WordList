588dd9276f8ee69681108097e2383a2d729c6527
Rafael <rafaeloliveiracds@gmail.com>
2023-02-21 17:08:43 -0300

    [ test ] Open and close perfil

--- frontend/components/pages/main/__tests__/perfil.spec.tsx
+++ frontend/components/pages/main/__tests__/perfil.spec.tsx
@@ -1,11 +1,20 @@
+import "@testing-library/jest-dom";
 import { act, fireEvent, render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { Perfil } from "../perfil";
+
+vi.mock("next/router", () => ({
+	useRouter() {
+		return {
+			query: "list01",
+		};
+	},
+}));
 
 describe("Perfil", () => {
 	it("open and close perfil menu", async () => {
 		render(<Perfil />);
 		act(() => fireEvent.click(screen.getByRole("btn-open-perfil")));
-		expect(await screen.findByRole("perfil-buttons")).not.toBeInTheDocument();
+		expect(await screen.findByRole("perfil-buttons")).toBeInTheDocument();
 	});
 });
