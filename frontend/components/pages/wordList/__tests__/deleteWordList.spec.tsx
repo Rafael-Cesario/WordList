8cc070790b14eeef997ba5621637b946185e7be3
Rafael <rafaeloliveiracds@gmail.com>
2023-02-23 17:38:07 -0300

    [ test ] delete wordList

--- frontend/components/pages/wordList/__tests__/deleteWordList.spec.tsx
+++ frontend/components/pages/wordList/__tests__/deleteWordList.spec.tsx
@@ -1,6 +1,6 @@
 import { QueriesWordList } from "../../../../services/queries/queriesWordList";
 import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, vi } from "vitest";
+import { beforeEach, describe, vi } from "vitest";
 import { DeleteWordList } from "../deleteWordList";
 
 vi.mock("next/router", () => ({
@@ -15,13 +15,29 @@ vi.mock("../../../../utils/hooks/useLocalData");
 vi.mock("../../../../services/queries/queriesWordList");
 
 describe("Delete wordList", () => {
-	it("ask for a confirmation before deleting the wordList", async () => {
+	beforeEach(() => {
 		render(<DeleteWordList />);
+	});
 
+	it("ask for a confirmation before deleting the wordList", async () => {
 		fireEvent.click(screen.getByRole("delete-btn"));
 		expect(await screen.findByRole("delete-btn-confirm")).toHaveTextContent(/Clique novamente para excluir/);
 
 		fireEvent.blur(screen.getByRole("delete-btn-confirm"));
 		expect(screen.queryByRole("delete-btn-confirm")).not.toBeInTheDocument();
+	});
+
+	it("Delete a wordList", () => {
+		const queriesWordList = new QueriesWordList();
+
+		fireEvent.click(screen.getByRole("delete-btn"));
+		fireEvent.click(screen.getByRole("delete-btn-confirm"));
+
+		expect(queriesWordList.deleteWordList).toHaveBeenCalledWith({
+			listName: "List01",
+			owner: "user",
+			wordListIndex: Number(0),
+			wordListStatus: "next",
+		});
 	});
 });
--- frontend/services/queries/__mocks__/queriesWordList.ts
+++ frontend/services/queries/__mocks__/queriesWordList.ts
@@ -3,3 +3,4 @@ import { vi } from "vitest";
 export const QueriesWordList = vi.fn();
 QueriesWordList.prototype.createWordList = vi.fn();
 QueriesWordList.prototype.changeWordListStatus = vi.fn();
+QueriesWordList.prototype.deleteWordList = vi.fn();
