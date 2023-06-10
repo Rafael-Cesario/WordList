e1bc0267cc67713fec98a241a34e7a70d473c74c
Rafael <rafaeloliveiracds@gmail.com>
2023-06-09 21:58:50 -0300

    font family

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,14 +1,20 @@
 import StyledComponentsRegistry from "@/lib/registry";
 import { GlobalStyle } from "@/styles/globalStyle";
+import { Roboto_Slab } from "next/font/google";
 
 export const metadata = {
 	title: "WordList",
 	description: "A website to learn a new language.",
 };
 
+const robotoSlab = Roboto_Slab({
+	display: "swap",
+	subsets: ["latin"],
+});
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<html lang="pt-br">
+		<html lang="pt-br" className={robotoSlab.className}>
 			<body>
 				<StyledComponentsRegistry>
 					<GlobalStyle />
