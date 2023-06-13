b5c1083c964f8380c98ee28501dd533b1e28605a
Rafael <rafaeloliveiracds@gmail.com>
2023-06-13 20:49:02 -0300

    create account form, capture values

--- Frontend/src/features/authentication/createAccount.tsx
+++ Frontend/src/features/authentication/createAccount.tsx
@@ -1,4 +1,5 @@
-import { SetStateAction } from "react";
+import { produce } from "immer";
+import { FormEvent, SetStateAction, useState } from "react";
 import { StyledForms } from "./styles/formsStyle";
 import { Password } from "./password";
 
@@ -9,14 +10,33 @@ interface ILogin {
 }
 
 export const CreateAccount = ({ props: { setFormName } }: ILogin) => {
+	const [values, setValues] = useState({
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+
+	const changeValue = (newValue: string, field: keyof typeof values) => {
+		const newState = produce(values, (draft) => {
+			draft[field] = newValue;
+		});
+
+		setValues(newState);
+	};
+
+	const submitForm = (e: FormEvent) => {
+		e.preventDefault();
+		console.log({ values });
+	};
+
 	return (
 		<StyledForms>
 			<h1 className="title">Criar Conta</h1>
 
-			<form className="fields">
-				<input type="text" placeholder="Email" />
-				<Password placeholder="Senha" />
-				<Password placeholder="Confirme sua senha" />
+			<form className="fields" onSubmit={(e) => submitForm(e)}>
+				<input type="text" placeholder="Email" onChange={(e) => changeValue(e.target.value, "email")} />
+				<Password placeholder="Senha" changeValue={changeValue} fieldName="password" />
+				<Password placeholder="Confirme sua senha" changeValue={changeValue} fieldName="confirmPassword" />
 				<button className="submit">Criar Conta</button>
 			</form>
 
--- Frontend/src/features/authentication/password.tsx
+++ Frontend/src/features/authentication/password.tsx
@@ -3,14 +3,16 @@ import { useState } from "react";
 
 interface PasswordProps {
 	placeholder: string;
+	changeValue: (value: string, fieldName: "password" | "confirmPassword") => void;
+	fieldName: "password" | "confirmPassword";
 }
 
-export const Password = ({ placeholder }: PasswordProps) => {
+export const Password = ({ placeholder, changeValue, fieldName }: PasswordProps) => {
 	const [isPasswordHide, setIsPasswordHide] = useState(true);
 
 	return (
 		<div className="password-input">
-			<input type={isPasswordHide ? "password" : "text"} placeholder={placeholder} />
+			<input type={isPasswordHide ? "password" : "text"} placeholder={placeholder} onChange={(e) => changeValue(e.target.value, fieldName)} />
 			<Image
 				onClick={() => setIsPasswordHide(!isPasswordHide)}
 				className="input-icon"
