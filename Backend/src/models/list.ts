3af19ca79c4cf29babc0ca31ce87a49f01475220
Rafael <rafaeloliveiracds@gmail.com>
2023-06-22 08:39:53 -0300

    [ Feat ] Word Model

--- Backend/src/models/list.ts
+++ Backend/src/models/list.ts
@@ -7,6 +7,23 @@ class ListClass {
 
 	@prop({ type: String, required: true, lowercase: true })
 	public name!: string;
+
+	@prop({ type: mongoose.Schema.Types.Array, required: true })
+	public words!: Word[];
+}
+
+class Word {
+	@prop({ type: String, required: true, lowercase: true })
+	public term!: string;
+
+	@prop({ type: String, required: true, lowercase: true })
+	public definitions!: string;
+
+	@prop({ type: Number, required: true })
+	public correctTimes!: number;
+
+	@prop({ type: Boolean, required: true })
+	public learned!: boolean;
 }
 
 export const ListModel = getModelForClass(ListClass, { options: { customName: "List" } });
