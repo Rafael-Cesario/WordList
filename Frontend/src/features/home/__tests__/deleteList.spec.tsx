8abe56db967f5ea33a895def646ce12cc1d8a612
Rafael <rafaeloliveiracds@gmail.com>
2023-06-26 14:32:25 -0300

    [ test ] delete list, send a notification if request return a error

--- Frontend/src/features/home/__tests__/deleteList.spec.tsx
+++ Frontend/src/features/home/__tests__/deleteList.spec.tsx
@@ -3,28 +3,41 @@ import { screen } from "@testing-library/react";
 import { renderHomePage } from "./__utils__/renderHomePage";
 import * as QueriesList from "@/hooks/useQueriesList";
 
-const mockedQueriesList = QueriesList as { useQueriesList: object };
-
 describe("Delete list", () => {
+	const mockedQueriesList = QueriesList as { useQueriesList: object };
 	const user = userEvent.setup();
 
-	it("Send a notification if user type wrong the list name", async () => {
-		mockedQueriesList.useQueriesList = () => ({
-			requestReadLists: () => ({
-				lists: [
-					{ _id: "123", userID: "123", name: "List01" },
-					{ _id: "321", userID: "321", name: "List02" },
-				],
-			}),
-		});
+	mockedQueriesList.useQueriesList = () => ({
+		requestReadLists: () => ({
+			lists: [
+				{ _id: "123", userID: "123", name: "List01" },
+				{ _id: "321", userID: "321", name: "List02" },
+			],
+		}),
 
-		await renderHomePage();
+		requestDeleteList: () => ({ error: "Hello, i'm a error" }),
+	});
+
+	const openDeleteListMenu = async () => {
 		await user.click(screen.getByText(/list02/i));
 		await user.click(screen.getByRole("show-delete-list"));
+	};
+
+	it("Send a notification if user type wrong the list name", async () => {
+		await renderHomePage();
+		await openDeleteListMenu();
 		await user.click(screen.getByRole("submit"));
 		expect(screen.getByRole("notification").getAttribute("type")).toBe("error");
 
 		await user.type(screen.getByRole("submit-input"), "Wrong Name");
+		await user.click(screen.getByRole("submit"));
+		expect(screen.getByRole("notification").getAttribute("type")).toBe("error");
+	});
+
+	it("Send a notification if the request return a error", async () => {
+		await renderHomePage();
+		await openDeleteListMenu();
+		await user.type(screen.getByRole("submit-input"), "list02");
 		await user.click(screen.getByRole("submit"));
 		expect(screen.getByRole("notification").getAttribute("type")).toBe("error");
 	});
