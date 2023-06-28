876aab3e270785dd304051627eabb3b303eb278b
Rafael <rafaeloliveiracds@gmail.com>
2023-06-28 19:40:01 -0300

    [ Style ] Add words Textarea

--- Frontend/src/features/list/addWords.tsx
+++ Frontend/src/features/list/addWords.tsx
@@ -4,29 +4,46 @@ import { useState } from "react";
 import { StyledAddWords } from "./styles/addWordsStyle";
 
 export const AddWords = () => {
-	const [menuAddWords, setMenuAddWords] = useState(false);
+	const [menuAddWords, setMenuAddWords] = useState<"" | "one" | "many">("");
+
+	const generateClass = (name: "one" | "many") => {
+		return menuAddWords === name ? "active" : "";
+	};
 
 	return (
 		<>
-			<button onClick={() => setMenuAddWords(true)} className="add-words-button">
+			<button onClick={() => setMenuAddWords("one")} className="add-words-button">
 				Adicionar palavras
 			</button>
 
 			{menuAddWords && (
 				<StyledAddWords>
 					<div className="container">
-						<button className="close">x</button>
+						<div className="menu">
+							<button onClick={() => setMenuAddWords("one")} className={generateClass("one")}>
+								Individual
+							</button>
+							<button onClick={() => setMenuAddWords("many")} className={generateClass("many")}>
+								Adicionar várias
+							</button>
+						</div>
+
+						<button className="close" onClick={() => setMenuAddWords("")}>
+							x
+						</button>
+
 						<h1 className="title">Adicione novas palavras</h1>
 
-						<input autoFocus={true} type="text" placeholder="Termo" className="term" />
-						<input type="text" placeholder="Definições" className="definitions" />
+						{menuAddWords === "one" && (
+							<>
+								<input autoFocus={true} type="text" placeholder="Termo" className="term" />
+								<input type="text" placeholder="Definições" className="definitions" />
+							</>
+						)}
+
+						{menuAddWords === "many" && <textarea></textarea>}
 
 						<button className="submit">Adicionar</button>
-
-						<div className="menu">
-							<button>Individual</button>
-							<button>Adicionar várias</button>
-						</div>
 					</div>
 				</StyledAddWords>
 			)}
--- Frontend/src/features/list/styles/addWordsStyle.tsx
+++ Frontend/src/features/list/styles/addWordsStyle.tsx
@@ -6,13 +6,12 @@ export const StyledAddWords = styled.div`
 	top: 0;
 	left: 0;
 	width: 100%;
-	height: 100%;
+	min-height: 100%;
 	background-color: #11111150;
 	backdrop-filter: blur(2px);
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	transform: translateY(-5%);
 
 	.close {
 		position: absolute;
@@ -49,6 +48,11 @@ export const StyledAddWords = styled.div`
 				padding: 10px 20px;
 				width: 15rem;
 				text-align: left;
+				background-color: ${theme.container};
+			}
+
+			.active {
+				background-color: ${theme.primary};
 			}
 
 			@media (max-width: 1400px) {
@@ -73,6 +77,29 @@ export const StyledAddWords = styled.div`
 
 		.submit {
 			margin: 2rem 0;
+		}
+
+		textarea {
+			resize: none;
+			min-height: 20vh;
+			border: 5px solid #05050520;
+			outline: 5px solid #05050520;
+			background-color: ${theme.background};
+			color: #ddd;
+
+			&::-webkit-scrollbar {
+				cursor: pointer;
+				background-color: ${theme.container};
+			}
+
+			&::-webkit-scrollbar-thumb {
+				background-color: #aaa;
+				border-radius: 2px;
+			}
+
+			&::-webkit-scrollbar-button {
+				background-color: ${theme.container};
+			}
 		}
 	}
 `;
