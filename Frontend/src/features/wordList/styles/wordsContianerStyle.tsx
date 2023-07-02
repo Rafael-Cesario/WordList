7d239cfc9244336ebd39bb76bdab455769a9acd8
Rafael <rafaeloliveiracds@gmail.com>
2023-07-02 20:09:16 -0300

    [ Style ] save changes

--- Frontend/src/features/wordList/styles/wordsContianerStyle.tsx
+++ Frontend/src/features/wordList/styles/wordsContianerStyle.tsx
@@ -16,10 +16,32 @@ export const StyledWordsContainer = styled.div`
 		grid-template-columns: 1fr 2fr;
 
 		.word {
+			font-size: 0.8rem;
+			font-weight: bold;
+			color: #ddd;
 			border-radius: 2px;
 			padding: 5px 10px;
 			background-color: ${theme.background};
 			margin: 5px;
+		}
+	}
+
+	.save {
+		position: fixed;
+		top: 0;
+		right: 0;
+		margin: 1rem;
+		font-size: 1rem;
+		font-weight: bold;
+		padding: 1rem 5rem;
+		background-color: ${theme.error};
+		box-shadow: 10px 10px 10px #11111120;
+		border: 1px solid #703030;
+		border-radius: 2px;
+
+		&:hover {
+			background-color: ${theme.container};
+			border: 1px solid #202020;
 		}
 	}
 `;
