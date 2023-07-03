8009d7b92b21e1704fb0455f634b005183e1dfbf
Rafael <rafaeloliveiracds@gmail.com>
2023-07-02 23:16:13 -0300

    Get one list from cache

--- Frontend/src/hooks/list/useOneList.tsx
+++ Frontend/src/hooks/list/useOneList.tsx
@@ -1,25 +1,40 @@
 import { StoreType } from "@/context/store";
 import { onListLoad } from "@/features/list/context/oneListSlice";
+import { client } from "@/services/client";
 import { RGetOneList } from "@/services/interfaces/list";
 import { QueriesList } from "@/services/queries/list";
-import { useQuery } from "@apollo/client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export const useOneList = (listID: string, userID: string) => {
-	const globalState = useSelector((state: StoreType) => state.oneList);
 	const queriesList = new QueriesList();
-	const variables = { listID, userID };
-	const { data, loading } = useQuery<RGetOneList>(queriesList.GET_ONE_LIST, { variables });
-	const list = data?.getOneList;
+	const globalState = useSelector((state: StoreType) => state.oneList);
 	const dispatch = useDispatch();
+
+	const [loading, setLoading] = useState(true);
+	const variables = { listID, userID };
+
+	const requestGetOneList = async () => {
+		const cache = client.readQuery<RGetOneList>({ query: queriesList.GET_ONE_LIST, variables });
+
+		if (cache?.getOneList) {
+			dispatch(onListLoad({ list: cache.getOneList }));
+			setLoading(false);
+			return;
+		}
+
+		const { data } = await client.query<RGetOneList>({ query: queriesList.GET_ONE_LIST, variables });
+		if (data) dispatch(onListLoad({ list: data.getOneList }));
+
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		requestGetOneList();
+	}, []);
 
 	// todo > if error send notification;
 	// todo > add loading to global State.
-
-	useEffect(() => {
-		list && dispatch(onListLoad({ list }));
-	}, [loading]);
 
 	return { list: globalState.list, loading };
 };
